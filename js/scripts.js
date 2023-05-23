e433104db6fb5c126acbd00afb8036ea1e294e1a
StanislavDitsan <ditsanstas@gmail.com>
2023-05-23 16:51:52 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-require('dotenv').config(); // Load environment variables from .env file
-const bootstrap = require('bootstrap');
-const nodemailer = require('nodemailer');
-
 window.addEventListener('DOMContentLoaded', event => {
+
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
@@ -10,14 +7,14 @@ window.addEventListener('DOMContentLoaded', event => {
             target: '#mainNav',
             offset: 74,
         });
-    }
+    };
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = Array.from(
+    const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.forEach(responsiveNavItem => {
+    responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
@@ -25,45 +22,8 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentYear').textContent = currentYear;
 });
 
-// Create a function to send the email
-async function sendEmail() {
-    try {
-        // Create a transport object using SMTP
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_ADDRESS,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
 
-        // Get the form data
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const phone = document.getElementById('phone').value;
-        const message = document.getElementById('message').value;
-
-        // Prepare the email message
-        const mailOptions = {
-            from: process.env.EMAIL_ADDRESS,
-            to: 'recipient@example.com',
-            subject: 'Message',
-            text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`
-        };
-
-        // Send the email
-        await transporter.sendMail(mailOptions);
-        console.log('Email sent successfully');
-
-        // Show success message
-        document.getElementById('submitButton').disabled = true;
-        document.getElementById('successMessage').classList.remove('d-none');
-    } catch (error) {
-        console.error('Error sending email:', error);
-        // Handle error here
-    }
-}
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
